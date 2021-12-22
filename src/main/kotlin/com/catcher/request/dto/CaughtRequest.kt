0b31b23fee5b670a699cafe27a738c7145814332
Shuqian Hon <honsq90+github@protonmail.com>
2021-12-22 17:09:14 +1100

    Add footer

--- src/main/kotlin/com/catcher/request/dto/CaughtRequest.kt
+++ src/main/kotlin/com/catcher/request/dto/CaughtRequest.kt
@@ -1,6 +1,9 @@
 package com.catcher.request.dto
 
+import java.util.*
+
 data class CaughtRequest(
+    val uuid: UUID = UUID.randomUUID(),
     val timestamp: String,
     val path: String,
     val method: String,
